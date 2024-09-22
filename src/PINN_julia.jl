7c9819c0a6ef1716ac32570090fa9da7a5b71d24
Nikita Budnikov <budnikov_nikita@list.ru>
2024-09-22 21:42:13 +0300

    Logarithm loss diagram added

--- src/PINN_julia.jl
+++ src/PINN_julia.jl
@@ -2,7 +2,7 @@
 using NeuralPDE, Lux, ModelingToolkit
 using Optimization, OptimizationOptimisers
 # Diagram of functions
-using Plots
+using Plots, LaTeXStrings
 import ModelingToolkit: Interval, infimum, supremum
 # Solution
 using Bessels
@@ -18,15 +18,16 @@ Dxx = Differential(x)^2
 eq = Dxx(y(x)) * x^2 + Dx(y(x)) * x + (x^2 - α^2) * y(x) ~ 0
 
 # Boundary conditions
-bcs = [y(0) ~ 1.0]
+bcs = [y(0) ~ 1.0,
+       Dx(y(0)) ~ 0.0]
 # Space and time domains
 domains = [x ∈ Interval(0.0, 10.0)]
 
 # Neural network
 inner = 16
-chain = Lux.Chain(Dense(1, inner, Lux.σ),
-                  Dense(inner, inner, Lux.σ),
-                  Dense(inner, inner, Lux.σ),
+chain = Lux.Chain(Dense(1, inner, sin),
+                  Dense(inner, inner, sin),
+                  Dense(inner, inner, sin),
                   Dense(inner, 1))
 
 # Discretization
@@ -35,9 +36,11 @@ discretization = PhysicsInformedNN(chain, strategy)
 
 @named pde_system = PDESystem(eq, bcs, domains, [x], [y(x)])
 prob = discretize(pde_system, discretization)
+loss = []
 
 # Callback function
 callback = function (p, l)
+    push!(loss, log10(l))
     println("Current loss is: $l")
     return false
 end
@@ -67,3 +70,10 @@ plot!(x_plot,
       ylims=(-1,1),
       xlims=(0,10))
 png("Bessel.png")
+
+p1 = plot(LinearIndices(loss),
+          loss,
+          label = "loss(epochs)",
+          xlabel = "epochs",
+          ylabel = L"log_{10}(loss)")
+png(p1, "loss.png")
