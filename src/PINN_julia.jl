ef9bbe3e8fbe5be2d297b8e3251ce2694cb0fc31
Nikita Budnikov <budnikov_nikita@list.ru>
2024-10-05 21:30:54 +0300

    Graphics improve

--- src/PINN_julia.jl
+++ src/PINN_julia.jl
@@ -13,15 +13,26 @@ using Bessels
 Dx = Differential(x)
 Dxx = Differential(x)^2
 
-α = 1
+# Consts
+const lr1 = 4000;
+const lr2 = 4000;
+const lr3 = 8000;
+const lr4 = 14000;
+const α = 0;
+const Grid = "uniform";
 
 # Bessel ODE
 eq = Dxx(y(x)) * x^2 + Dx(y(x)) * x + (x^2 - α^2) * y(x) ~ 0
 
 # Boundary conditions
-bcs = [y(0) ~ 0.0,
-       Dx(y(0)) ~ 0.5]
-# Space and time domains
+if(α == 0)
+    bcs = [y(0) ~ 1.0,
+           Dx(y(0)) ~ 0.0]
+elseif(α == 1)
+    bcs = [y(0) ~ 0.0,
+           Dx(y(0)) ~ 0.5]
+end
+# Space domain
 domains = [x ∈ Interval(0.0, 10.0)]
 
 # Neural network
@@ -32,8 +43,11 @@ chain = Lux.Chain(Dense(1, inner, sin),
                   Dense(inner, 1))
 
 # Discretization
-strategy = GridTraining(0.05)
-loss_type = NonAdaptiveLoss(pde_loss_weights = 1.0, bc_loss_weights = 1.0, additional_loss_weights = 0.0)
+if(Grid == "uniform")
+    strategy = GridTraining(0.05)
+elseif(Grid == "bn") 
+end
+loss_type = NonAdaptiveLoss(pde_loss_weights = 1.0, bc_loss_weights = 0.3, additional_loss_weights = 0.0)
 discretization = PhysicsInformedNN(chain, strategy, adaptive_loss=loss_type)
 
 @named pde_system = PDESystem(eq, bcs, domains, [x], [y(x)])
@@ -47,15 +61,13 @@ callback = function (p, l)
     return false
 end
 
-res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.01); callback = callback, maxiters = 2000)
+res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.01); callback = callback, maxiters = lr1)
 prob = remake(prob, u0 = res.u)
-res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.001); callback = callback, maxiters = 2000)
+res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.001); callback = callback, maxiters = lr2)
 prob = remake(prob, u0 = res.u)
-res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.0001); callback = callback, maxiters = 2000)
+res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.0001); callback = callback, maxiters = lr3)
 prob = remake(prob, u0 = res.u)
-res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.00001); callback = callback, maxiters = 6000)
-prob = remake(prob, u0 = res.u)
-res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.000001); callback = callback, maxiters = 8000)
+res = Optimization.solve(prob, OptimizationOptimisers.Adam(0.00001); callback = callback, maxiters = lr4)
 
 phi = discretization.phi
 
@@ -79,8 +91,11 @@ plot!(x_plot,
       xlabel = L"X",
       ylabel = L"Y",
       ylims=(-1,1),
-      xlims=(0,10))
-png("../images/Bessel_1.png")
+      xlims=(0,10),
+      size = (1600, 900),
+      left_margin=10Plots.mm,
+      bottom_margin=10Plots.mm)
+png("../images/Bessel_$(α)_$(Grid).png")
 
 p1 = plot(LinearIndices(loss),
           loss,
@@ -88,9 +103,14 @@ p1 = plot(LinearIndices(loss),
           xlabel = L"epochs",
           ylabel = L"log_{10}(loss)",
           yticks = -5:1:3,
-          xticks = 0:1000:20000,
-          size = (1200, 800),
+          xticks = 0:2000:30000,
+          size = (1600, 900),
           left_margin=10Plots.mm,
           bottom_margin=10Plots.mm,
           formatter=:plain)
-png(p1, "../images/loss_1.png")
+vline!([0, lr1, lr1+lr2, lr1+lr2+lr3], line = :dash)
+annotate!(0+700, 2, text(L"η = 0.01"))
+annotate!(lr1+800, 2, text(L"η = 0.001"))
+annotate!(lr1+lr2+900, 2, text(L"η = 0.0001"))
+annotate!(lr1+lr2+lr3+1000, 2, text(L"η = 0.00001"))
+png(p1, "../images/loss_$(α)_$(Grid).png")
